4fa5c06c13acc6bfdcb3b6254a2131d0e705113b
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-07-26 14:36:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,7 +63,7 @@ function draw()
   
   con = Matter.Constraint.create({
     pointA:{x:200,y:20},
-    bodyB:{x:200,y:20},
+    bodyB:ball,
     pointB:{x:0,y:0},
     length:100,
     stiffness:0.1
